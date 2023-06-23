6034c9447a5aba19a9cd94d87166239fea472f6e
RaihanaAm <raihana.amrani999@gmail.com>
2023-06-23 14:34:13 +0100

    menu animation done

--- public/js/app.js
+++ public/js/app.js
@@ -9,27 +9,68 @@ const delet = () => {
 }
 window.onscroll = () => {
 
-        let ids = scrollY
-        // console.log(ids);
-        if (ids >=0 && ids<500 ) {
-            delet()
-            itemNav[0].firstElementChild.classList.add("select");
-        } else if (ids >=505 && ids<1909) {
-            delet()
-            itemNav[1].firstElementChild.classList.add("select");
-        } else if (ids >=1909 && ids<3211) {
-            delet()
-            itemNav[2].firstElementChild.classList.add("select");
-        } else if (ids >=3211 && ids<4705) {
-            delet()
-            itemNav[4].firstElementChild.classList.add("select");
-        } else if (ids >=4705) {
-            delet()
-            itemNav[7].firstElementChild.classList.add("select");
-        } 
+    let ids = scrollY
+    // console.log(ids);
+    if (ids >= 0 && ids < 500) {
+        delet()
+        itemNav[0].firstElementChild.classList.add("select");
+    } else if (ids >= 505 && ids < 1909) {
+        delet()
+        itemNav[1].firstElementChild.classList.add("select");
+    } else if (ids >= 1909 && ids < 3211) {
+        delet()
+        itemNav[2].firstElementChild.classList.add("select");
+    } else if (ids >= 3211 && ids < 4705) {
+        delet()
+        itemNav[4].firstElementChild.classList.add("select");
+    } else if (ids >= 4705) {
+        delet()
+        itemNav[7].firstElementChild.classList.add("select");
     }
+}
 
+// ********** menu
+let textTab = document.querySelectorAll(".texttabs");
+let menuHiddin = document.querySelectorAll(".menuHiddin");
+const menu = (num) => {
+    for (let index = 0; index < menuHiddin.length; index++) {
+        let element = menuHiddin[index];
+        element.classList.add("d-none")
+    }
+    menuHiddin[num].classList.remove("d-none")
+}
+const menuHover = (element) => {
+    for (let index = 0; index < textTab.length; index++) {
+         let item = textTab[index];
+        item.classList.remove("menuSelect")
+    }
+    element.classList.add("menuSelect")
+}
+for (let index = 0; index < textTab.length; index++) {
+    let element = textTab[index];
+    element.addEventListener("click", () => {
+        console.log(element);
+        switch (element.firstElementChild.textContent) {
+            case "Starters":
+                menuHover(element)
+                menu(0);
+                break;
+            case "Breakfast":
+                menuHover(element)
+                menu(1);
+                break;
+            case "Lunch":
+                menuHover(element)
+                menu(2);
+                break;
+            case "Dinner":
+                menuHover(element)
+                menu(3);
+                break;
+        }
+    })
 
+}
 
 
 
