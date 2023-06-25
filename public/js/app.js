b237395db22c2da91abb3177f9a6808dae23c287
RaihanaAm <raihana.amrani999@gmail.com>
2023-06-25 15:45:01 +0100

    carrpussel event done

--- public/js/app.js
+++ public/js/app.js
@@ -11,22 +11,32 @@ window.onscroll = () => {
 
     let ids = scrollY
     // console.log(ids);
-    if (ids >= 0 && ids < 500) {
+    if (ids >= 0 && ids < 480) {
         delet()
-        itemNav[0].firstElementChild.classList.add("select");
-    } else if (ids >= 505 && ids < 1909) {
+        itemNav[0].firstElementChild.classList.add("select"); // home
+    } else if (ids >= 480 && ids < 1940) {
         delet()
-        itemNav[1].firstElementChild.classList.add("select");
-    } else if (ids >= 1909 && ids < 3211) {
+        itemNav[1].firstElementChild.classList.add("select"); //about
+    } else if (ids >= 1940 && ids < 3211) {
         delet()
-        itemNav[2].firstElementChild.classList.add("select");
-    } else if (ids >= 3211 && ids < 4705) {
+        itemNav[2].firstElementChild.classList.add("select"); //menu
+    } else if (ids >= 3211 && ids < 3988) {
         delet()
-        itemNav[4].firstElementChild.classList.add("select");
-    } else if (ids >= 4705) {
+        itemNav[3].firstElementChild.classList.add("select"); //events
+    } else if (ids >= 3988 && ids < 4500) {
         delet()
-        itemNav[7].firstElementChild.classList.add("select");
-    }
+        itemNav[4].firstElementChild.classList.add("select"); //chefs
+    } else
+        if (ids >= 5449 && ids < 5915) {
+            delet()
+            itemNav[5].firstElementChild.classList.add("select");
+        } else if (ids >= 5915) {
+            delet()
+            itemNav[7].firstElementChild.classList.add("select");
+        }
+        else {
+            delet()
+        }
 }
 
 // ********** menu
@@ -78,9 +88,9 @@ let watch = document.querySelector(".watch");
 let btnPlay = document.querySelector(".video");
 let bodu = document.querySelector(".body")
 let xmark = document.querySelector(".xmark");
-let link_youtube =watch.querySelectorAll("iframe");
-let video_play2 =document.querySelector(".vdp");
-const play=()=>{
+let link_youtube = watch.querySelectorAll("iframe");
+let video_play2 = document.querySelector(".vdp");
+const play = () => {
     bodu.classList.add("overflow");
     watch.classList.remove("d-none");
 }
@@ -88,16 +98,16 @@ btnPlay.addEventListener("click", play);
 video_play2.addEventListener("click", play);
 
 
-    // ^close btn
+// ^close btn
 xmark.addEventListener("click", () => {
     bodu.classList.remove("overflow");
     watch.classList.add("d-none")
 })
 // ^^arrows
-let left =document.querySelector(".fa-chevron-left");
-let right =document.querySelector(".fa-chevron-right");
+let left = document.querySelector(".fa-chevron-left");
+let right = document.querySelector(".fa-chevron-right");
 
-const nextt=()=>{
+const nextt = () => {
 
 
 
@@ -109,26 +119,26 @@ const nextt=()=>{
             element.classList.add("d-none");
             break;
         }
-        if (!link_youtube[link_youtube.length-1].classList.contains("d-none")) {
+        if (!link_youtube[link_youtube.length - 1].classList.contains("d-none")) {
             let next_eleme = link_youtube[0]
             next_eleme.classList.remove("d-none")
-            link_youtube[link_youtube.length-1].classList.add("d-none");
+            link_youtube[link_youtube.length - 1].classList.add("d-none");
             break;
-            
+
         }
     }
 }
-const previous=()=>{
+const previous = () => {
 
 
     for (let index = 0; index < link_youtube.length; index++) {
         let element = link_youtube[index];
         if (!link_youtube[0].classList.contains("d-none")) {
-            let prev_elem = link_youtube[link_youtube.length-1]
+            let prev_elem = link_youtube[link_youtube.length - 1]
             prev_elem.classList.remove("d-none")
             link_youtube[0].classList.add("d-none");
             break;
-            
+
         }
         if (!element.classList.contains("d-none")) {
             let prev_elem = element.previousElementSibling
@@ -139,14 +149,47 @@ const previous=()=>{
     }
 }
 
-left.addEventListener("click",previous)
-right.addEventListener("click",nextt)
+left.addEventListener("click", previous)
+right.addEventListener("click", nextt)
 
 
 
-// ** carroussel
-// let content = document.getElementById('images');
-// let parents = content.parentNode;
-// console.log(parents);
-// // parents.insertBefore(content, parents.firstChild);
+// ** carroussel event 
+let points = document.querySelectorAll(".point");
+let carr = document.querySelector(".event_images");
+
+const drags = (num) => {
+    carr.scrollLeft = num
+}
+
+const redc = () => {
+    for (let i = 0; i < points.length; i++) {
+        let element = points[i];
+        element.classList.remove("redp")
+    }
+}
+
+for (let i = 0; i < points.length; i++) {
+    let element = points[i];
+    element.addEventListener("click", () => {
+        redc()
+        element.classList.add("redp");
+    })
+}
+
+points[0].addEventListener("click", drags.bind(null, 0))
+points[1].addEventListener("click", drags.bind(null, 450))
+points[2].addEventListener("click", drags.bind(null, 900))
+
+
+// points[0].addEventListener("click",()=>{
+//     order(2,3)
+//     order(0,-1)
+// })
+
+// document.getElementById("myRedDIV").style.order = "4";
+// document.getElementById("myBlueDIV").style.order = "3";
+// document.getElementById("myGreenDIV").style.order = "1";
+// document.getElementById("myPinkDIV").style.order = "2";
+
 
